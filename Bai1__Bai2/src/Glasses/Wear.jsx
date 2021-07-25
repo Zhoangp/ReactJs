e393aff72d55076d49c084691f4ee1e1adaf4191
Nguyen Hoang Phuc <nguyenhoangphucc7@gmail.com>
2021-07-26 00:09:07 +0700

    update wear.jsx

--- Bai1__Bai2/src/Glasses/Wear.jsx
+++ Bai1__Bai2/src/Glasses/Wear.jsx
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 
 class Wear extends Component {
     render() {
-        const { url } = this.props.selected;
+        const { url, name, desc} = this.props.selected;
         return (
             <div style={{zIndex: "10",position: "absolute", left: "43.8%", top: "31%"}}>
                 <img src = {url} style={{width: "180px"}}></img>
+                <div style={{position: "absolute", left: "-29.5%", right: "-29.5%", top: "250%", background: "#f9ba2685"}}>
+                <h3 style= {{color: "#9b68fb"}}>{name}</h3>
+                <p>{desc}</p>
+                </div>
             </div>
         );
     }
